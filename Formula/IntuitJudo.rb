95a34f0b1a251dbb67a9237f4eacf24517d9356e
Michael Salaverry <michael_salaverry@intuit.com>
2020-10-15 09:44:52 +0300

    fix: audit changes

--- Formula/IntuitJudo.rb
+++ Formula/IntuitJudo.rb
@@ -9,16 +9,16 @@ class Intuitjudo < Formula
 
   depends_on "node"
 
+  resource("testyaml") do
+    url "https://raw.githubusercontent.com/intuit/judo/master/test-examples/simple-test-suite/hello-world.yml"
+    sha256 "0b6101f0145cc24569709595adeefc23e4bd8340fafe6a8ec4ea2c09e5a308bb"
+  end
+
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
-  resource("testyaml") do
-    url "https://raw.githubusercontent.com/intuit/judo/master/test-examples/simple-test-suite/hello-world.yml"
-    sha256 "0b6101f0145cc24569709595adeefc23e4bd8340fafe6a8ec4ea2c09e5a308bb"
-  end
-  
   test do
     resource("testyaml").stage do
       assert_match "JUDO TESTS COMPLETE", shell_output("#{bin}/judo hello-world.yml")
